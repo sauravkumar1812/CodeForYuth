8444d3b02258740a702e3cf75479cdf495f181f5
saurav1812 <shorya181299@gmail.com>
2023-04-22 00:34:27 +0530

    intial changes

--- lib/profile/profilepage.dart
+++ lib/profile/profilepage.dart
@@ -1,11 +1,9 @@
-
+import 'package:code4yth/logout/logout.dart';
 import 'package:code4yth/message/messagepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:code4yth/profiledetail/details.dart';
-
-
 
 class profilepage extends StatelessWidget {
   const profilepage({super.key});
@@ -15,53 +13,197 @@ class profilepage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-                    leading: new IconButton(
-                      
-                      icon: Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: new Icon(Icons.arrow_back_ios, color: Colors.black,size: 25,),
-                      ), onPressed: () => Navigator.pop(context,
-      MaterialPageRoute(builder: (context) => messagepage()))
-                    ),
-                    
-
+        leading: new IconButton(
+            icon: Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: new Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+                size: 25,
+              ),
+            ),
+            onPressed: () => Navigator.pop(context,
+                MaterialPageRoute(builder: (context) => messagepage()))),
         backgroundColor: Colors.white,
-      title: Text("Profile",style: TextStyle(color: Colors.black)),
-      centerTitle: true,
-      actions: <Widget>[
-        IconButton(
-          icon: Icon(
-            Icons.more_horiz,
-            color: Colors.black,
-            size: 35,
-          ),
-          onPressed: () {
-            // do something
-          },
-        )
-      ],
-    ),
+        title: Text("Dashboard", style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.more_horiz,
+              color: Colors.black,
+              size: 35,
+            ),
+            onPressed: () {
+              // do something
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => logout()));
+            },
+          )
+        ],
+      ),
       backgroundColor: Colors.white,
-      body: Column(
-        
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.30,
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 150.0),
+          child: Text(
+            "Welcome Back Komal !",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height * 0.25,
+          width: MediaQuery.of(context).size.width ,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/profile/Nature.png'))),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 250.0, bottom: 10),
+          child: Text(
+            "What's the mood?",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width * 0.22,
+              color: Color.fromARGB(255, 101, 130, 144),
+            ),
+          ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width * 0.22,
+              color: Color.fromARGB(255, 101, 130, 144),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width * 0.22,
+              color: Color.fromARGB(255, 101, 130, 144),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.1,
+              width: MediaQuery.of(context).size.width * 0.22,
+              color: Color.fromARGB(255, 101, 130, 144),
+            ),
+          ),
+        ]),
+        Row(
+mainAxisAlignment: MainAxisAlignment.spaceAround, 
+          children: [
+             Text(
+               "Mon",
+               style: TextStyle(
+                 fontSize: 20,
+               ),
+             ),
+            Text(
+              "Tue",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),    Text(
+              "Wed",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),    Text(
+              "Thu",
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+
+        Container(
+          width: MediaQuery.of(context).size.width * 0.90,
+          height: MediaQuery.of(context).size.height * 0.08,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Row(
+                children: [
+                 Icon(
+                Icons.arrow_back_ios,
+                color: Color.fromARGB(255, 255, 254, 254),
+                size: 20,
+              ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 80),
+                    child: Text(
+                      "Let's do a quick round",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ],
+              ),
+              
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.white),
+                  backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 121, 147, 159)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)))),
+            ),
+          ),
+          
+        ),
+         Padding(
+          padding: const EdgeInsets.only(right: 260.0, ),
+          child: Text(
+            "Daily journal",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10,left: 20,right: 20,bottom: 10),
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.22,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/profile/Nature.png'))),
-                  
+              borderRadius: BorderRadius.circular(20),
+              color: Color.fromARGB(255, 230, 229, 229),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, top: 15),
+              child: Text(
+                "Tell us how your day today......",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w400,
+                  // height: 1.7,
+                  color: Color.fromARGB(255, 40, 40, 40),
+                ),
+              ),
+            ),
           ),
-           Padding(
-             padding: const EdgeInsets.only(bottom: 20,top: 20,),
-             child: details(),
-           
-           ) ,
-       
-            
-        ]),
- 
+        ),
+        //  Padding(
+        //    padding: const EdgeInsets.only(bottom: 20,top: 20,),
+        //    child: details(),
+
+        //  ) ,
+      ]),
     );
   }
 }
