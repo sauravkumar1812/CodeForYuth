a935b163e1a3d2fe1fe5798b66b4488b6ba3c5c2
saurav1812 <shorya181299@gmail.com>
2023-04-24 01:13:27 +0530

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-           primarySwatch : Colors.blue,
-        ),
+       
       debugShowCheckedModeBanner: false,
      
       home:LoginPage(),
